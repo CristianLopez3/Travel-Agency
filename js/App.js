e1711a307b5301f12abcb248e4c3bb8cd34d39fd
Cristian-Lopez <126030346+CristianLopez3@users.noreply.github.com>
2023-10-09 18:54:28 -0500

    add loader and navbar

--- js/App.js
+++ js/App.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", (e) => {
 
-  const loader = document.querySelector('.loader');
-  
+
 })
+
+window.addEventListener('load', function(){
+  const $loader = document.querySelector('.loader');
+  const $principal = document.getElementById('principal-content');
+
+  setTimeout(function() {
+    console.log('carga completa');
+    $loader.classList.add('minimizar');
+    $principal.classList.remove('hidden');
+  }, 0); // 5000 milisegundos (5 segundos) CAMBIARLA
+});
