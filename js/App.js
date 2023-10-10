92a526287da38add50dfbec07525e2c8e7ea2e3c
Cristian-Lopez <126030346+CristianLopez3@users.noreply.github.com>
2023-10-09 23:29:56 -0500

    header already

--- js/App.js
+++ js/App.js
@@ -1,15 +1,50 @@
 document.addEventListener("DOMContentLoaded", (e) => {
 
+  // SWIPPER CONFIGURATIONS
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    effect: 'cube',
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+/*    autoplay: {
+      delay: 1000,
+      pauseOnMouseEnter: true,
+      disableInteraction: false,
+    },*/
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    shadow: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+    slideShadows: true,
+  });
+
+
+  // LOADER CONFIGURATIONS
+  window.addEventListener('load', function(){
+    const $loader = document.querySelector('.loader');
+    const $principal = document.getElementById('principal-content');
+  
+    setTimeout(function() {
+      console.log('carga completa');
+      $loader.classList.add('minimizar');
+      $principal.classList.remove('hidden');
+    }, 0); // 5000 milisegundos (5 segundos) CAMBIARLA
+  });
+
+  // ANOTHER CONFIGURATION
 
 })
 
-window.addEventListener('load', function(){
-  const $loader = document.querySelector('.loader');
-  const $principal = document.getElementById('principal-content');
 
-  setTimeout(function() {
-    console.log('carga completa');
-    $loader.classList.add('minimizar');
-    $principal.classList.remove('hidden');
-  }, 0); // 5000 milisegundos (5 segundos) CAMBIARLA
-});
+
